fcf54d66cbd08f01cb38453ad4cba6359055e4e5
PATEL PRATHAM <patelpratham429@gmail.com>
2024-10-12 22:06:43 +0530

    commit

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.urls import reverse
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinLengthValidator(1),MaxLengthValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+
+
+    def get_absolute_url(self):
+        return reverse("book-details", args=[self.id])
+    
     def __str__(self):
         return f"{self.title} ({self.rating}) {self.author} {self.is_bestselling}"
     
---
+++ book_store/book_outlet/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("",views.index),
+    path("<id>",views.book_details,name="book-details")
+]
